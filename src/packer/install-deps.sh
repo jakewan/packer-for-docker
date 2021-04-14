35f7eb782c0a91170e743f3a6948eb7e83246f34
Jacob Wan <jacobwan840@gmail.com>
2021-04-14 15:41:01 -0700

    Able to build and tag Docker image

---
+++ src/packer/install-deps.sh
@@ -0,0 +1,11 @@
+# Packer
+curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
+apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+
+# Docker
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -y packer docker-ce docker-ce-cli containerd.io
---
+++ src/packer/spec.pkr.hcl
@@ -0,0 +1,23 @@
+source "docker" "main" {
+  image  = "debian:stable-20210408-slim"
+  commit = true
+}
+
+build {
+  sources = ["source.docker.main"]
+  provisioner "file" {
+    source      = "install-deps.sh"
+    destination = "/tmp/"
+  }
+  provisioner "shell" {
+    inline = [
+      "ls -l /tmp",
+      "DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates gnupg lsb-release",
+      "bash /tmp/install-deps.sh",
+    ]
+  }
+  post-processor "docker-tag" {
+    repository = "frostedcarbon/packer-for-docker"
+    tag        = ["0.1"]
+  }
+}
